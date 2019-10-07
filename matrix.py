fb57b6ea2e4780a87ad82f2bead48b92cbb2d90f
flucey <47129058+flucey@users.noreply.github.com>
2019-10-06 22:06:20 -0700

    Add files via upload

--- matrix.py
+++ matrix.py
@@ -1,13 +1,7 @@
 import numpy as np
-from numpy.linalg import matrix_power
-P=np.matrix([[0.25, 0.40],[0.75, 0.60]])
-print(P)
-N=np.matrix([[50],[50]])
-print(N)
-N=P*N
-print(N)
-P10=matrix_power(P,10)
-N=P10*N
-print(N)
-N=P10*N
-print(N)
+from numpy.linalg import inv
+A=np.matrix([[1,2,3],[2,4,5],[3,5,6]])
+print(A)
+B=inv(A)
+C=A*B
+print(C)
